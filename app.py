04785ea5a5468a9cdd199692789e0796666c295f
Vincent Fontaine <ftechhelp@gmail.com>
2025-03-05 09:39:08 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from JoshServerOllama import JoshServerOllama
+from Ollama import Ollama
 
 # Create the client
-client = JoshServerOllama()
+client = Ollama()
 #client.chat("What is the best way to learn and practice parkour?")
 #client.prompt_template("What is the best way to learn and practice {activity}?", {"activity": "parkour"})
 client.chat_prompt_template("You are an expert and teacher.", "What is the best way to learn and practice {activity}?", {"activity": "parkour"})
